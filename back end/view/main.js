36d656cfb8cf22ee9d612967982255e36afd82ea
amrwheab <amr.wheab2020@gmail.com>
2021-01-25 21:35:43 +0200

    adding client auth

--- back end/view/main.js
+++ back end/view/main.js
@@ -2843,7 +2843,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    server: 'http://localhost:3000'
+    server: 'http://ebookforyou.herokuapp.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
